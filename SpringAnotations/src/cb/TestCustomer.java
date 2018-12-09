940e95f8224764c2b9b5898c22521f2f2e1b5df2
George Verroiopoulos <george.verroiopoulos@gmail.com>
2018-12-09 13:45:19 +0200

    example for  autowired annotation

--- SpringAnotations/src/cb/TestCustomer.java
+++ SpringAnotations/src/cb/TestCustomer.java
@@ -12,11 +12,11 @@ public class TestCustomer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-//		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		ApplicationContext context2 = new ClassPathXmlApplicationContext("autobeans.xml");
-//		Customer_Compoment customer_Compoment = (Customer_Compoment) context.getBean("myCustomer");
-//		System.out.println(customer_Compoment.getCust_name() + "\t" + customer_Compoment.getCust_id() +
-//				customer_Compoment.getCust_address());
+		Customer_Compoment customer_Compoment = (Customer_Compoment) context.getBean("myCustomer");
+		System.out.println(customer_Compoment.getCust_name() + "\t" + customer_Compoment.getCust_id() +
+				customer_Compoment.getCust_address());
 		
 		Customer_Autowired customer_Autowired = (Customer_Autowired) context2.getBean("autoCustomer");
 		System.out.println(customer_Autowired.getCust_name() + "\t" + customer_Autowired.getCust_id() +
